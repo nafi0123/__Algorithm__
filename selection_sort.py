7243dcb023af46d0b5a6fe13ec80647f749e8467
Nafi Mahmud Bukhari <143020717+nafi0123@users.noreply.github.com>
2023-12-07 19:52:09 +0600

    Create selection_sort.py

---
+++ selection_sort.py
@@ -0,0 +1,17 @@
+class SelectionSort:
+    def __init__(self,arr):
+        self.arr=arr
+    def sort_(self,array):
+        for i in range(len(array)):
+            mini=i
+            for j in range(i+1,len(array)):
+                if array[j] < array[mini]:
+                    mini = j
+            if mini !=i:
+                array[i],array[mini]=array[mini],array[i]
+        return array
+
+if __name__ == "__main__":
+    arr=[1,50,3,2,0,800,300]
+    ob=SelectionSort(arr)
+    print(ob.sort_(arr))
