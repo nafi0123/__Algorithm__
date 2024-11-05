1824a37e35e8cbc504897b2037beb7a6e9792421
Nafi Mahmud Bukhari <143020717+nafi0123@users.noreply.github.com>
2024-11-05 18:59:52 +0600

    Create Disjoint Set | Union by Size | Path Compression.py

---
+++ Disjoint Set | Union by Size | Path Compression.py
@@ -0,0 +1,44 @@
+class DisjointSet:
+    def __init__(self, n):
+        self.parent = list(range(n + 1))
+        self.size = [1] * (n + 1)
+
+    def findUPar(self, node):
+        if node == self.parent[node]:
+            return node
+        self.parent[node] = self.findUPar(self.parent[node]) 
+        return self.parent[node]
+
+
+    def unionBySize(self, u, v):
+        ulp_u = self.findUPar(u)
+        ulp_v = self.findUPar(v)
+        if ulp_u == ulp_v:
+            return
+        if self.size[ulp_u] < self.size[ulp_v]:
+            self.parent[ulp_u] = ulp_v
+            self.size[ulp_v] += self.size[ulp_u]
+        else:
+            self.parent[ulp_v] = ulp_u
+            self.size[ulp_u] += self.size[ulp_v]
+
+
+if __name__ == "__main__":
+    ds = DisjointSet(7)
+    ds.unionBySize(1, 2)
+    ds.unionBySize(2, 3)
+    ds.unionBySize(4, 5)
+    ds.unionBySize(6, 7)
+    ds.unionBySize(5, 6)
+
+
+    if ds.findUPar(3) == ds.findUPar(7):
+        print("Same")
+    else:
+        print("Not same")
+
+    ds.unionBySize(3, 7)
+    if ds.findUPar(3) == ds.findUPar(7):
+        print("Same")
+    else:
+        print("Not same")
