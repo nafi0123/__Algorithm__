581ea36633b3530e59532d8ad824ed929b31874b
Nafi Mahmud Bukhari <143020717+nafi0123@users.noreply.github.com>
2025-01-21 20:50:38 +0600

    Update Floyd_Warshall_Algorithm.py

--- Floyd_Warshall_Algorithm.py
+++ Floyd_Warshall_Algorithm.py
@@ -1,27 +1,40 @@
 class Solution:
     def shortest_distance(self, matrix):
         n = len(matrix)
+        
+
         for i in range(n):
             for j in range(n):
                 if matrix[i][j] == -1:
-                    matrix[i][j] = float('inf')
+                    matrix[i][j] = int(1e9)
                 if i == j:
                     matrix[i][j] = 0
-        
 
-        
 
-V = 4
-matrix = [[-1] * V for _ in range(V)]
-matrix[0][1] = 2
-matrix[1][0] = 1
-matrix[1][2] = 3
-matrix[3][0] = 3
-matrix[3][1] = 5
-matrix[3][2] = 4
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j])
 
-obj = Solution()
-obj.shortest_distance(matrix)
 
-for row in matrix:
-    print(" ".join(map(str, row)))
+        for i in range(n):
+            for j in range(n):
+                if matrix[i][j] == int(1e9):
+                    matrix[i][j] = -1
+
+
+if __name__ == "__main__":
+    V = 4
+    matrix = [[-1] * V for _ in range(V)]
+    matrix[0][1] = 2
+    matrix[1][0] = 1
+    matrix[1][2] = 3
+    matrix[3][0] = 3
+    matrix[3][1] = 5
+    matrix[3][2] = 4
+
+    obj = Solution()
+    obj.shortest_distance(matrix)
+
+    for row in matrix:
+        print(" ".join(map(str, row)))
